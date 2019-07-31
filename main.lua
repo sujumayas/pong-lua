a92abf8d5f22666461d7a4dc5ad4bcbee56e22f7
sujumayas <sujumayas@gmail.com>
2019-07-31 14:15:33 -0500

    pong-2 custom-fonts, rectangles and clear plus extra new-version notes in readme

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@
     -- Main Program -- 
 
     Based on Colton Ogden repo:
-    https://github.com/games/pong
+    https://github.com/cs50/gd50/tree/master/pong
     cogden@cs50.harvard.edu
 
 
@@ -18,35 +18,34 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
-
---[[ 
-    Runs when the game first starts up, only once; used to initialize the game
+--[[
+    Runs when the game first starts up, only once; used to initialize the game.
 ]]
 function love.load()
-    -- use nearest-neighbor filtering on upscaling and downscaling to make real the low-res feeling.      
     love.graphics.setDefaultFilter('nearest', 'nearest')
-    
-    -- Replaces window.setMode function and sets a virtual resolution with new w/h 
+
+    -- more "retro-looking" font object we can use for any text
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- set LÖVE2D's active font to the smallFont obect
+    love.graphics.setFont(smallFont)
+
+    -- initialize window with virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
-    -- Without push implementation
-    -- love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-    --     fullscreen = false,
-    --     resizable = false,
-    --     vsync = true
-    -- })
 end
 
-
 --[[
-  Keyboard handling, called by LOVE2D each frame;
-  passes in the key we pressed so we can access.
+    Keyboard handling, called by LÖVE2D each frame; 
+    passes in the key we pressed so we can access.
 ]]
 function love.keypressed(key)
+    -- keys can be accessed by string name
     if key == 'escape' then
+        -- function LÖVE gives us to terminate application
         love.event.quit()
     end
 end
@@ -56,21 +55,31 @@ end
 --[[
     Called after update by LOVE2D, used to draw anything 
 ]]
-
 function love.draw()
-    -- Begin rendering at virtual resolution
+    -- begin rendering at virtual resolution
     push:apply('start')
-    
 
-    
-    love.graphics.printf(
-        'Hello Pong',            -- Text to render           
-        0,                       -- Starting X (0 since we are going to center it)
-        VIRTUAL_HEIGHT / 2 - 6,   -- Starting Y (halfway down the screen)                  
-        VIRTUAL_WIDTH,            -- number of pixels to center within (entire screen)     
-        'center'                 -- alignment mode, can be 'center', 'left', 'right'.
-    )
-    
+    -- clear the screen with a specific color; in this case, a color similar
+    -- to some versions of the original Pong
+    love.graphics.clear(0.2, 0.21, 0.3, 1) --Check readme > now its 0-1 values, not 
+
+    -- draw welcome text toward the top of the screen
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    --
+    -- paddles are simply rectangles we draw on the screen at certain points,
+    -- as is the ball
+    --
+
+    -- render first paddle (left side)
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    -- render second paddle (right side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- render ball (center)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
     -- end rendering at virtual resolution
     push:apply('end')
 end
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ Inspired by CS50 Harvard University online Classes
 
 ## Repo from Harvard: 
 
-[https://github.com/games/pong](https://github.com/games/pong)
+[https://github.com/cs50/gd50/tree/master/pong](https://github.com/cs50/gd50/tree/master/pong)
 
 ## What is Lua
 
@@ -49,7 +49,7 @@ So we represent every coordinate with the X axis as first parameter and the Y ax
                              (0, 1)
 
 
-### Pong requirements
+## Pong requirements
 
 - Draw shapes to the screen (paddles and ball)
 - Control 2D position of paddles based on input
@@ -82,4 +82,28 @@ Other functions that we will use in this game:
 
 - `love.event.quit()` > Simple function that terminates the application. 
 
+### PONG-2: The rectangle update important functions
 
+- `love.graphics.newFont(path,size)` > Loads a font file into memory at a specific path, setting it to a specific size, and storing it in an object we can use to globally change the current active font that LOVE2D is using to render text (functioning like a state machine).
+- `love.graphics.setFont(font)` > Sets LOVE2D's currently active font (of which there can only be one at a time) to a passed-in font object that we can create using `love.graphics.newFont()`.
+- `love.graphics.clear(r, g, b, a)` > Wipes the entire screen with a color defined by an RGBA set, each component of which being from 0-255. 
+- `love.graphics.rectangle(mode, x, y, width, height)` > Draws a rectangle onto the screen using whichever our active color is (`love.graphics.setColor`, white is default). Mode can be set to 'fill' or 'line', which result in a filled or outlined rectangle, respectively, and the other four parameters are its position and size dimensions. 
+
+- As Found on Youtube:  
+```
+Here was everything I needed for this to work in Love 11.2:
+
+- method getPixelScale needs to be changed to getDPIScale
+- setColor uses values between 0-1 instead of 0-255 so old values will cause white screen.
+- push: setColor(1,1,1)
+- main: clear(0.2, 0.21, 0.3, 1) -- this is a guessed conversion
+- note: fps colors added in 7 will work as-is, but 255 can be changed to 1 for good practice
+
+Anyone on a Retina screen may get a half-sized screen, so on push:101 set "_PSCALE" to "self._highdpi and 1" (getDPIScale throws it off)
+
+Once you update the push file with the above updates, you can use that version in the rest of the folders.
+
+There are lots of additions to 11 to DRY things up, but the above is what I needed to complete and run all of the code from the GitHub repo. 
+
+https://www.youtube.com/watch?v=jZqYXSmgDuM&list=PLWKjhJtqVAbluXJKKbCIb4xd7fcRkpzoz&index=2&t=1504s
+```
